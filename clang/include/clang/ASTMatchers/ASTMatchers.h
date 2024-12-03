c643f779e219a8560d67df98031e3ced659ada27
Rashmi Mudduluru <r_mudduluru@apple.com>
2024-12-03 12:26:20 -0800

    [ASTMatchers] AST matcher support for ObjC pointers (#117021)
    Add `ObjCInterfaceDecl` to the list of types supported by the `hasType`
    and `hasDeclaration` matchers, `ObjCObjectPointerType` to the list of
    types supported by `pointee`.
    These AST matcher improvements will help the new WebKit checker for
    unsafe casts
    ([https://github.com/llvm/llvm-project/pull/114606](https://github.com/llvm/llvm-project/pull/114606))
    match on unsafe Objective-C pointer casts.

--- clang/include/clang/ASTMatchers/ASTMatchers.h
+++ clang/include/clang/ASTMatchers/ASTMatchers.h
@@ -3997,7 +3997,7 @@ AST_POLYMORPHIC_MATCHER_P_OVERLOAD(
 AST_POLYMORPHIC_MATCHER_P_OVERLOAD(
     hasType,
     AST_POLYMORPHIC_SUPPORTED_TYPES(Expr, FriendDecl, ValueDecl,
-                                    CXXBaseSpecifier),
+                                    CXXBaseSpecifier, ObjCInterfaceDecl),
     internal::Matcher<Decl>, InnerMatcher, 1) {
   QualType QT = internal::getUnderlyingType(Node);
   if (!QT.isNull())
@@ -7227,7 +7227,8 @@ extern const AstTypeMatcher<RValueReferenceType> rValueReferenceType;
 AST_TYPELOC_TRAVERSE_MATCHER_DECL(
     pointee, getPointee,
     AST_POLYMORPHIC_SUPPORTED_TYPES(BlockPointerType, MemberPointerType,
-                                    PointerType, ReferenceType));
+                                    PointerType, ReferenceType,
+                                    ObjCObjectPointerType));
 
 /// Matches typedef types.
 ///
